a94959c55e1e3c1a7ae1e23c2bc9f36258ffc6f4
Adriano Ayala <adriano.ayc@gmail.com>
2025-09-25 00:09:13 -0400

    Prisma - Información completa del recibo

--- section_2-preparacion_proyecto/report-server/src/store-reports/store-reports.controller.ts
+++ section_2-preparacion_proyecto/report-server/src/store-reports/store-reports.controller.ts
@@ -7,8 +7,11 @@ export class StoreReportsController {
   constructor(private readonly storeReportsService: StoreReportsService) {}
 
   @Get('orders/:orderId')
-  getOrderReport(@Res() response: Response, @Param('orderId') orderId: string) {
-    const pdfDoc = this.storeReportsService.getOrderByIdReport(orderId);
+  async getOrderReport(
+    @Res() response: Response,
+    @Param('orderId') orderId: string,
+  ) {
+    const pdfDoc = await this.storeReportsService.getOrderByIdReport(+orderId);
 
     response.setHeader('Content-Type', 'application/pdf');
     pdfDoc.info.Title = 'Order-Report';
