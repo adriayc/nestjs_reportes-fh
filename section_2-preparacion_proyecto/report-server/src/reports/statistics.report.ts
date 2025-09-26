e9fbe1f7889f2adb0df5c82a0e2c623aec7ed3b4
Adriano Ayala <adriano.ayc@gmail.com>
2025-09-26 16:29:54 -0400

    Gráfica líneal

--- section_2-preparacion_proyecto/report-server/src/reports/statistics.report.ts
+++ section_2-preparacion_proyecto/report-server/src/reports/statistics.report.ts
@@ -1,6 +1,7 @@
 import type { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { getDonutChart } from './charts/donut.chart';
 import { headerSection } from './sections/header.section';
+import { getLineChart } from './charts/line.chart';
 
 interface TopCountry {
   country: string;
@@ -16,13 +17,16 @@ interface ReportOptions {
 export const getStatisticsReport = async (
   options: ReportOptions,
 ): Promise<TDocumentDefinitions> => {
-  const donutChart = await getDonutChart({
-    entries: options.topCountries.map((c) => ({
-      label: c.country,
-      value: c.customers,
-    })),
-    position: 'left',
-  });
+  const [donutChart, lineChart] = await Promise.all([
+    getDonutChart({
+      entries: options.topCountries.map((c) => ({
+        label: c.country,
+        value: c.customers,
+      })),
+      position: 'left',
+    }),
+    getLineChart(),
+  ]);
 
   const docDefinition: TDocumentDefinitions = {
     header: headerSection({
@@ -59,6 +63,11 @@ export const getStatisticsReport = async (
             },
           },
         ],
+      },
+      {
+        image: lineChart,
+        width: 500,
+        margin: [0, 20],
       },
     ],
   };
