315ea2b8234b3936e4a0e656455b1f13ea08393f
Adriano Ayala <adriano.ayc@gmail.com>
2025-09-27 16:51:56 -0400

    Personalización de tablas - Parte 2

--- section_2-preparacion_proyecto/report-server/src/reports/community.report.ts
+++ section_2-preparacion_proyecto/report-server/src/reports/community.report.ts
@@ -6,12 +6,51 @@ export const getCommunityReport = (): TDocumentDefinitions => {
       fontSize: 10,
     },
     content: [
+      // Log - Dirección - Número de orden
       {
         columns: [
           { image: 'src/assets/tucan-code-logo.png', width: 50 },
           {
             text: 'Forest Admin Community SAP\n RUT: 44.123.1233\n Camino montaña Km 16\n Telefono: +56 2 23455668',
             alignment: 'center',
+          },
+          {
+            width: 140,
+            alignment: 'right',
+            layout: 'borderBlue',
+            table: {
+              widths: ['auto'],
+              body: [
+                [
+                  {
+                    layout: 'noBorders',
+                    table: {
+                      body: [
+                        ['No.', '123-456'],
+                        ['Fecha', '2025-10-27'],
+                        ['Versión', '2025-001'],
+                      ],
+                    },
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+
+      // Horizontal line
+      {
+        margin: [0, 5],
+        canvas: [
+          {
+            type: 'line',
+            x1: 0,
+            y1: 5,
+            x2: 515,
+            y2: 5,
+            lineWidth: 2,
+            lineColor: '#3a4546',
           },
         ],
       },
