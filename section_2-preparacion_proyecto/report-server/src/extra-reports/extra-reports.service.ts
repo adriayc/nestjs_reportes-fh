3a72d44006db2d7cd61cd10d9ec4a55e4147acde
Adriano Ayala <adriano.ayc@gmail.com>
2025-09-27 15:20:04 -0400

    HTML complejo

--- section_2-preparacion_proyecto/report-server/src/extra-reports/extra-reports.service.ts
+++ section_2-preparacion_proyecto/report-server/src/extra-reports/extra-reports.service.ts
@@ -4,22 +4,30 @@ import { PrinterService } from 'src/printer/printer.service';
 import { getHtmlContent } from 'src/helpers/html-to-pdfmake';
 import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { headerSection } from 'src/reports/sections/header.section';
+import { footerSection } from 'src/reports/sections/footer.section';
 
 @Injectable()
 export class ExtraReportsService {
   constructor(private readonly printerService: PrinterService) {}
 
   getHtmlReport() {
-    const html = fs.readFileSync('src/reports/html/basic-01.html', 'utf-8');
+    // const html = fs.readFileSync('src/reports/html/basic-01.html', 'utf-8');
+    // const html = fs.readFileSync('src/reports/html/basic-02.html', 'utf-8');
+    const html = fs.readFileSync('src/reports/html/basic-03.html', 'utf-8');
 
-    const content = getHtmlContent(html);
+    const content = getHtmlContent(html, {
+      client: 'Adriano Ayala',
+      title: 'Curso de Nest.js',
+    });
 
     const docDefinition: TDocumentDefinitions = {
       header: headerSection({
         title: 'HTML to PDFMake',
         subTitle: 'Convertir HTML a PDFMake',
       }),
+      footer: footerSection,
       pageMargins: [40, 110, 40, 60],
+      //   content: ['Hola mundo', content],
       content: content,
     };
 
--- section_2-preparacion_proyecto/report-server/src/helpers/html-to-pdfmake.ts
+++ section_2-preparacion_proyecto/report-server/src/helpers/html-to-pdfmake.ts
@@ -1,7 +1,22 @@
 import htmlToPdfmake from 'html-to-pdfmake';
 import { JSDOM } from 'jsdom';
 
-export const getHtmlContent = (html: string) => {
+//  Record<string, string> is the same sa { [key: string]: string }
+interface ContentReplacer {
+  [key: string]: string;
+}
+
+export const getHtmlContent = (
+  html: string,
+  replacers: ContentReplacer = {},
+) => {
+  Object.entries(replacers).forEach(([key, value]) => {
+    const key1 = `{{ ${key} }}`;
+    const key2 = `{{${key}}}`;
+
+    html = html.replaceAll(key1, value).replaceAll(key2, value);
+  });
+
   const { window } = new JSDOM();
 
   return htmlToPdfmake(html, { window });
